2d91a64cd960892696e388f3cdbc8550bc9e760f
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-19 20:34:18 +0600

    dynamic data load on booking with available slots with React Query done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -15,14 +16,54 @@ app.get('/', async (req, res) => {
 // DB_USER: doctorsPortalDB
 // DB_PASSWORD: uiG3m42Inmpx3UFw
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.mmmt3qa.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mmmt3qa.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+
+        const database = client.db('DoctorsPortal');
+        const appointmentOptions = database.collection('appointmentOptions');
+        const bookingAppointments = database.collection('bookingAppointments');
+
+
+        // sending all the appointmentOptions
+        app.get('/appointmentOptions', async (req, res) => {
+            // Getting all the appointmentOptions
+            const query = {};
+            const cursor = appointmentOptions.find(query);
+            const options = await cursor.toArray();
+
+            // collecting already booked appointments regarding each date the appointment was taken on
+            const date = req.query.date;
+            const bookingQuery = { appointmentDate: date };
+            const alreadyBooked = await bookingAppointments.find(bookingQuery).toArray();
+
+            // Sorting the available booking slots based on each treatment options
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(booked => booked.treatmentName === option.name);
+                const bookedSlots = optionBooked.map(booked => booked.selectedSlot);
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+                option.slots = remainingSlots; // set the slots of every treatment option to remainingSlots
+            })
+
+            res.send(options);
+        })
+
+        // sending booking data to the database after client posts new booking
+        app.post('/bookings', async (req, res) => {
+            const doc = req.body;
+            const result = await bookingAppointments.insertOne(doc);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch((err) => console.log(err))
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
